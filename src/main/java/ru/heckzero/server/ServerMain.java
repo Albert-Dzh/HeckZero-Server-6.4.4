be6fda87b1d199dbc40d82f32b7decaeff1b2e1a
Mike Zagraevsky <mmzagr@gmail.com>
2021-06-08 15:39:30 +0300

    some minor changes

--- src/main/java/ru/heckzero/server/ServerMain.java
+++ src/main/java/ru/heckzero/server/ServerMain.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 public class ServerMain {
     private static final Logger logger = LogManager.getFormatterLogger();
     public static final String CONF_DIR = "conf";
-    public static final String CONF_FILE = "mb-dup-check.xml";
+    public static final String CONF_FILE = "heckzero.xml";
     public static final String VERSION = "1.0";
     public static XMLConfiguration config;
 
