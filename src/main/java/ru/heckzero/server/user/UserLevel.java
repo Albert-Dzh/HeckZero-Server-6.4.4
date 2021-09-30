7b0fa0395e0367e73f75072abfb46642abe3e44a
Albert-Dzh <tryhardgopru@gmail.com>
2021-09-30 15:51:36 +0300

    getExpLastLvl/getExpNextLvl

--- src/main/java/ru/heckzero/server/user/UserLevel.java
+++ src/main/java/ru/heckzero/server/user/UserLevel.java
@@ -22,9 +22,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Table(name = "users_level")
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, region = "default")
 public class UserLevel {
-    private static final Logger logger = LogManager.getFormatterLogger();
-    private static final List<UserLevel> userLevels = new ArrayList<>();                                                                    //store all data from db here for further usage
-    private static AtomicBoolean initialized = new AtomicBoolean(false);
+    private static final Logger logger              = LogManager.getFormatterLogger();
+    private static final List<UserLevel> userLevels = new ArrayList<>();                                                                            //store all data from db here for further usage
+    private static final AtomicBoolean initialized  = new AtomicBoolean(false);
 
     @Id
     private int exp;
@@ -36,7 +36,7 @@ public class UserLevel {
 
     public UserLevel() { }
 
-    private static void ensureInitialized() {                                                                                               //make sure list of UserLevel objects initialized
+    private static void ensureInitialized() {                                                                                                       //make sure list of UserLevel objects initialized
         if (initialized.compareAndSet(false, true)) {
             try (Session ses = ServerMain.sessionFactory.openSession()) {
                 List<UserLevel> levels = ses.createQuery("select u from UserLevel u", UserLevel.class).list();
@@ -48,15 +48,9 @@ public class UserLevel {
                 userLevels.notifyAll();
             }
         }
-        catch (NoResultException ex) { logger.error("can't load user level table: %s:%s", ex.getClass().getSimpleName(), ex.getMessage()); }
     }
 
-<<<<<<<<< Temporary merge branch 1
-    //get UserLevel state by User
-    private static UserLevel getUserStatus(User usr) {
-=========
     private static UserLevel getUserStatus(User usr) {                                                                                              //get curr User state (UserLevel) by his "exp" status
->>>>>>>>> Temporary merge branch 2
         ensureInitialized();
         synchronized (userLevels) {
             while (userLevels.isEmpty()) {
@@ -88,11 +82,7 @@ public class UserLevel {
     public static int getMaxExpPvE(User u)  { return u.getParamInt(User.Params.man) == 0 ? getUserStatus(u).exp_pve_woman : getUserStatus(u).exp_pve_man; }
     public static int getMaxExpPvP(User u)  { return u.getParamInt(User.Params.man) == 0 ? getUserStatus(u).exp_pvp_woman : getUserStatus(u).exp_pvp_man; }
 
-<<<<<<<<< Temporary merge branch 1
-    public static int getQuestDiffRevard(User u, int diffLvl) {
-=========
     public static int getQuestDiffReward(User u, int diffLvl) {
->>>>>>>>> Temporary merge branch 2
         return switch (diffLvl) {
             default -> getUserStatus(u).quest_diff1;
             case 2  -> getUserStatus(u).quest_diff2;
@@ -102,58 +92,36 @@ public class UserLevel {
         };
     }
 
-    private static int getExpStatus(int userLvl) {              // вернуть кол-во опыта по уровню ("нижняя ступенька")
-        return userLevels.stream()                              // поток
-                .filter(ulvl -> ulvl.level == userLvl)          // оставить только нужный уровень (с его промежуточными "ступеньками")
-                .min(Comparator.comparingInt(o -> o.exp))       // взять наименьшее из выборки
-                .orElseGet(UserLevel::new)                      // или болванку
-                .exp;                                           // получить поле опыта
+    private static int getExpStatus(int userLvl) {
+        return userLevels.stream()
+                .filter(ulvl -> ulvl.level == userLvl)
+                .min(Comparator.comparingInt(o -> o.exp))
+                .orElseGet(UserLevel::new)
+                .exp;
     }
 
     @Override
     public String toString() {
-        return "UserLevel{" +
-<<<<<<<<< Temporary merge branch 1
-                "exp=" + exp +
-                ", level=" + level +
-                ", stat=" + stat +
-                ", maxHPMan=" + max_hp_man +
-                ", maxPsyMan=" + max_psy_man +
-                ", expPvEMan=" + exp_pve_man +
-                ", expPvPMan=" + exp_pvp_man +
-                ", maxHPWoman=" + max_hp_woman +
-                ", maxPsyWoman=" + max_psy_woman +
-                ", expPvEWoman=" + exp_pve_woman +
-                ", expPvPWoman=" + exp_pvp_woman +
-                ", maxrank=" + maxrank +
-                ", perkpoints=" + perkpoints +
-                ", statup=" + statup +
-                ", qestDiff1=" + quest_diff1 +
-                ", qestDiff2=" + quest_diff2 +
-                ", qestDiff3=" + quest_diff3 +
-                ", qestDiff4=" + quest_diff4 +
-                ", qestDiff5=" + quest_diff5 +
-=========
-                "exp="              + exp +
-                ", level="          + level +
-                ", stat="           + stat +
-                ", maxHPMan="       + max_hp_man +
-                ", maxPsyMan="      + max_psy_man +
-                ", expPvEMan="      + exp_pve_man +
-                ", expPvPMan="      + exp_pvp_man +
-                ", maxHPWoman="     + max_hp_woman +
+        return "UserLevel{"         +
+                "exp="              + exp           +
+                ", level="          + level         +
+                ", stat="           + stat          +
+                ", maxHPMan="       + max_hp_man    +
+                ", maxPsyMan="      + max_psy_man   +
+                ", expPvEMan="      + exp_pve_man   +
+                ", expPvPMan="      + exp_pvp_man   +
+                ", maxHPWoman="     + max_hp_woman  +
                 ", maxPsyWoman="    + max_psy_woman +
                 ", expPvEWoman="    + exp_pve_woman +
                 ", expPvPWoman="    + exp_pvp_woman +
-                ", maxrank="        + maxrank +
-                ", perkpoints="     + perkpoints +
-                ", statup="         + statup +
-                ", qestDiff1="      + quest_diff1 +
-                ", qestDiff2="      + quest_diff2 +
-                ", qestDiff3="      + quest_diff3 +
-                ", qestDiff4="      + quest_diff4 +
-                ", qestDiff5="      + quest_diff5 +
->>>>>>>>> Temporary merge branch 2
+                ", maxrank="        + maxrank       +
+                ", perkpoints="     + perkpoints    +
+                ", statup="         + statup        +
+                ", qestDiff1="      + quest_diff1   +
+                ", qestDiff2="      + quest_diff2   +
+                ", qestDiff3="      + quest_diff3   +
+                ", qestDiff4="      + quest_diff4   +
+                ", qestDiff5="      + quest_diff5   +
                 '}';
     }
 }
